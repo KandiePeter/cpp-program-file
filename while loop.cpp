9f38ddc8f75e12450313ddaf860b9a6076126451
KandiePeter <kandiepeter845@gmail.com>
2024-09-25 16:22:43 +0300

    while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,17 @@
+//while loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=1, sum=0;
+    while(i>0, i<=100)
+    {
+        cout<<i<<endl;
+        i++;
+        sum=sum+i;
+    }
+    
+    cout<<"the sum is "<<sum<<endl;
+
+    return 0;
+}
