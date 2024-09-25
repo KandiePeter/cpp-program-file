f50e7613cd2e094246463c343e96b2a11d8941d1
KandiePeter <kandiepeter845@gmail.com>
2024-09-25 17:03:12 +0300

    for loop.cpp

---
+++ for loop.cpp
@@ -0,0 +1,14 @@
+// for loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, sum=0;
+    for(i=1; i<=100; i++){
+        cout<<i<<endl;
+        sum=sum+1;
+    }
+ cout<<"the sum is "<<sum<<endl;
+
+    return 0;
+}
